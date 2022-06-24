7c792bb9270ee84d15b5f9700516bd59c310333f
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-06-24 09:34:02 +0530

    Add files via upload

---
+++ Basic Program/To find character for given ascii value.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+  char ch ='\0';
+  printf("\n Enter a ascii character =>");
+  scanf("%c",&ch);
+ 
+  printf("\n Ascii value of given character %d=%c",ch,ch);
+  
+  getch();
+  return 0;
+}
+
