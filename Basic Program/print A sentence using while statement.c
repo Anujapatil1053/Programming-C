8fdb32d7eff8dc5aa0d123ab50bb098dd5a5008d
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-07-10 21:42:10 +0530

    Add files via upload

---
+++ Basic Program/print A sentence using while statement.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+  int N=0;
+  while(N<5)
+  {
+    printf("\n INDIA");
+    N++;
+  }
+  printf("\n\nProcess Finish");
+  getch();
+  return 0;
+}
