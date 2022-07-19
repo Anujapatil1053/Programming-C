bde790fae41fbae7a8051979209b5aa8ee41d5ba
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-07-19 21:31:09 +0530

    Add files via upload

---
+++ Array/Array Demo. c.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  int Arr[ 5 ] = { 33 , 44 , 55 , 77 ,99};
+  
+  printf( " \n Value of 101 Element = %d", Arr[0] );
+  
+  printf( " \n Value of 102 Element = %d", Arr[1] );
+  
+  printf( " \n Value of 103 Element = %d", Arr[2] );
+  
+  printf( " \n Value of 104 Element = %d", Arr[3] );
+  
+  printf( " \n Value of 105  Element = %d", Arr[4] );
+  
+  getch();
+  
+  return 0;
+
+}
