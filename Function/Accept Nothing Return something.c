9c31fbb79655b7133cb4076e7b16a27bfbf5ebeb
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-08-04 12:57:50 +0530

    Update Accept Nothing Return something.c

--- Function/Accept Nothing Return something.c
+++ Function/Accept Nothing Return something.c
@@ -4,23 +4,27 @@
 int Add();
 int main()
 {
-  int Sum = 0;
-  Add(Sum);
-  getch();
-  return 0;
+                  int Sum = 0;
+  
+                  Add(Sum);
+  
+                  getch();
+  
+                  return 0;
   
 }
 int Add()
 {
-  int N1 = 0, N2 = 0, Dsum = 0;
+                  int N1 = 0, N2 = 0, Dsum = 0;
+
+                  printf("\n Enter Any Two Number =>");
+                  scanf("%d%d",&N1,&N2);
+
+                  Dsum = N1 + N2;
+
+                  printf("\n Addition of%d+%d=%d",N1,N2,Dsum );
+
+                  getch();
   
-  printf("\n Enter Any Two Number =>");
-  scanf("%d%d",&N1,&N2);
-  
-  Dsum = N1 + N2;
-  
-  printf("\n Addition of%d+%d=%d",N1,N2,Dsum);
-  
-  getch();
-  return Dsum;
+                  return Dsum;
 }
