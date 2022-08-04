2f9b530f6b42c940b2a627526f2152b29c0fc39f
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-08-04 13:00:48 +0530

    Update Reverse number with function.c

--- Function/Reverse number with function.c
+++ Function/Reverse number with function.c
@@ -4,29 +4,29 @@
 int Reverse_Number(int);
 int main()
 {
-  int No = 0;
-  
-  printf("\n Enter Any Number =>");
-  scanf("%d",&No);
-  
-  printf("Reverse of Given No %d",Reverse_Number(No));
-  getch();
-  return 0;
+                      int No = 0;
+
+                      printf("\n Enter Any Number =>");
+                      scanf("%d",&No);
+
+                      printf("Reverse of Given No %d",Reverse_Number(No));
+                      getch();
+                      return 0;
   
 }
 
 int Reverse_Number(Num)  
 {
-  int Rev = 0, Dig = 0;
-  
-   while(Num!=0)
-  {
-    Dig = Num%10;
-    Num = Num/10;
-    Rev = (Rev*10)+Dig;
-  }
-  
-  
-  getch();
-  return Rev;
+                      int Rev = 0, Dig = 0;
+
+                       while(Num!=0)
+                      {
+                        Dig = Num%10;
+                        Num = Num/10;
+                        Rev = (Rev*10)+Dig;
+                      }
+
+
+                      getch();
+                      return Rev;
 }  
