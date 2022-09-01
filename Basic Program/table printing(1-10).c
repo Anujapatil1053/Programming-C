e2786953647ecb388ade05d64452ecabe38f571c
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-09-01 13:45:34 +0530

    Update table printing(1-10).c

--- Basic Program/table printing(1-10).c
+++ Basic Program/table printing(1-10).c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include<conio.h>
+
 int main()
 {
-  int No1=0, i=0;
+              int No1 = 0, i = 0;
+
+              printf(" \n Enter A Number = > ");
+              scanf( " %d ",&No1 );
+
+              for( i = 1 ; i <= 10; i++)
+              {
+                printf( " \n\t %d", i*No1 );
+              }
+              getch();
   
-  printf("\n Enter A Number = > ");
-  scanf("%d",&No1);
-  
-  for(i=1;i<=10;i++)
-  {
-    printf("\n\t %d",i*No1);
-  }
-  getch();
-  return 0 ;
+              return 0 ;
 }
