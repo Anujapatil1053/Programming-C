32f1a27694ddb728037764033015f31f94899f99
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-09-01 13:43:47 +0530

    Update write a program accept number from user and check whether it is even or odd.c

--- Basic Program/write a program accept number from user and check whether it is even or odd.c
+++ Basic Program/write a program accept number from user and check whether it is even or odd.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <conio.h>
+
 int main()
 {
   int No=0;
-  printf("\n Enter Any Number = >");
-  scanf("%d",&No);
   
-  if(No%2==0)
+  printf (" \n Enter Any Number = >" );
+  scanf ( " %d ",&No );
+  
+  if ( No % 2 == 0 )
   {
-    printf("Given Number is Even ");
+    printf( "\n  Given Number is Even " );
     
   }
-  else
+  
   {
-    printf("Given Number is Odd");
+    printf( " Given Number is Odd " );
   }
   
-  getch();
+  getch(); 
+  
   return 0;
 }
