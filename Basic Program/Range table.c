3e0c3125e2e7584536f62dbbb14a6b69999a4a38
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-09-02 12:26:50 +0530

    Update Range table.c

--- Basic Program/Range table.c
+++ Basic Program/Range table.c
@@ -3,18 +3,19 @@
 
 int main()
 {
-  int R = 0,C = 0;
-  
-  for( R = 1; R <= 10; R++ )
-  {
-    for( C = 7; C <= 12; C++)
-    {
-      printf( " %d " ,R * C);
-    }
-    
-    printf("\n");
-    
-    getch();
-    return 0;
-  }
+      int R = 0,C = 0;
+
+      for( R = 1; R <= 10; R++ )
+      {
+        for( C = 7; C <= 12; C++)
+        {
+          printf( " %d " ,R * C);
+        }
+
+        printf("\n");
+
+        getch();
+        
+        return 0;
+      }
 }
