883fd74528b35713021b3489f9d563d58c0f6dd9
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-09-02 13:03:10 +0530

    Update Pointer Arithmetic Decrement Integral Constant.c

--- Pointer/Pointer Arithmetic Decrement Integral Constant.c
+++ Pointer/Pointer Arithmetic Decrement Integral Constant.c
@@ -3,24 +3,24 @@
 
 int main()
 {
-    int Num [5] = { 15, 59, 88, 27, 65 };
+                int Num [5] = { 15, 59, 88, 27, 65 };
+
+                int *ptr = &Num [4];
+
+                printf( " \n Base Address Of Array Is = %d ",Num);
+
+                printf( " \n Value In Pointer Is = %d ",ptr);
+
+                printf( " \n Value Of Array Element Where Pointer Pointing = %d ",*ptr);
+
+                getch();
+
+                ptr = ptr - 3;
     
-    int *ptr = &Num [4];
-    
-    printf( " \n Base Address Of Array Is = %d ",Num);
-    
-    printf( " \n Value In Pointer Is = %d ",ptr);
-    
-    printf( " \n Value Of Array Element Where Pointer Pointing = %d ",*ptr);
-    
-    getch();
-    
-    ptr = ptr - 3;
-    
-    
-    printf( " \n  New Value In Pointer Is = %d ",ptr);
-    
-    printf( " \n Value Of Array Element Where Pointer Pointing = %d ",*ptr);
+
+                printf( " \n  New Value In Pointer Is = %d ",ptr);
+
+                printf( " \n Value Of Array Element Where Pointer Pointing = %d ",*ptr);
     
     
 }
