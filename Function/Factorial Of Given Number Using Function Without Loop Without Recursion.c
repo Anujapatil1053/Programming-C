c51c34013caa33300c5ee3e9501d423ebcbef1ca
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-09-02 12:37:48 +0530

    Update Factorial Of Given Number Using Function Without Loop Without Recursion.c

--- Function/Factorial Of Given Number Using Function Without Loop Without Recursion.c
+++ Function/Factorial Of Given Number Using Function Without Loop Without Recursion.c
@@ -7,32 +7,32 @@ long int Factorial ( int );
 
 int main()
 {
-  int No = 0;
-  
-  printf (" \n Enter A Number To Find It's Factorial :\n ");
-  scanf ( " %d ",&No );
-  
-  printf ( " \n Factorial Of Given Number %d Is = %ld ",No, Factorial ( No ));
-  
-  getch();
-  
-  return 0;
+        int No = 0;
+
+        printf (" \n Enter A Number To Find It's Factorial :\n ");
+        scanf ( " %d ",&No );
+
+        printf ( " \n Factorial Of Given Number %d Is = %ld ",No, Factorial ( No ));
+
+        getch();
+
+        return 0;
 }
 
 long int Factorial ( int Num )
 {
-  long int Fac = 1;
-  
-  Next : 
-  
-  if ( Num > 1 )
-  {
-    Fac = Fac * Num;
-    Num--;
-    
-    goto Next;
-  }
-  
-  return Fac;
+        long int Fac = 1;
+
+        Next : 
+
+        if ( Num > 1 )
+        {
+              Fac = Fac * Num;
+              Num--;
+
+              goto Next;
+        }
+
+        return Fac;
 }
 
