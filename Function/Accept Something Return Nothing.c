1dff0bb7b71e2694ed551c20c70a9c2a22fbe35c
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-09-02 12:36:46 +0530

    Update Accept Something Return Nothing.c

--- Function/Accept Something Return Nothing.c
+++ Function/Accept Something Return Nothing.c
@@ -5,28 +5,29 @@ void Sum(int,int);
 
 int main()
 {
-  int No1 = 0, No2 = 0;
+        int No1 = 0, No2 = 0;
+
+        printf(" \n Enter First Number =>" );
+        scanf( " %d ",&No1 );
+
+        printf( " \n Enter Second Number =>" );
+        scanf( " %d ",&No2 );
+
+        Sum( No1, No2 );
+
+        getch();
   
-  printf(" \n Enter First Number =>");
-  scanf( "%d",&No1);
-  
-  printf("\n Enter Second Number =>");
-  scanf("%d",&No2);
-  
-  Sum(No1, No2);
-  
-  getch();
-  return 0;
+        return 0;
 }
-void Sum(int Num1, int Num2)
+void Sum(  int Num1, int Num2 )
 {
-  int Dsum = 0;
-  
-  Dsum = Num1 + Num2;
-  
-  printf("\n Addition of %d+%d=%d",Num1,Num2,Dsum);
-  
-  getch();
+        int Dsum = 0;
+
+        Dsum = Num1 + Num2;
+
+        printf( " \n Addition of %d+%d=%d",Num1,Num2,Dsum );
+
+        getch();
   
   
 }
