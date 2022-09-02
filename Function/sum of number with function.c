1f4a3fe075f5cc177c546702567ab4ecd61021a7
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-09-02 12:40:48 +0530

    Update sum of number with function.c

--- Function/sum of number with function.c
+++ Function/sum of number with function.c
@@ -4,29 +4,33 @@
 int Sum_Of_Number(int);
 int main()
 {
-  int No = 0;
+        int No = 0;
+
+        printf("\n Enter Any Number =>");
+        scanf("%d",&No);
+
+        printf("Sum of Number %d",Sum_Of_Number(No));
+
+
+        getch();
   
-  printf("\n Enter Any Number =>");
-  scanf("%d",&No);
-  
-  printf("Sum of Number %d",Sum_Of_Number(No));
-  
-  
-  getch();
-  return 0;
+        return 0;
 }
 
 int Sum_Of_Number(Num)
 {
-  int Sum = 0, Dig = 0;
+      int Sum = 0, Dig = 0;
+
+      while( Num != 0)
+      {
+        Dig = Num % 10;
+        
+        Num = Num / 10;
+        
+        Sum = Sum + Dig;
+      }
+      getch();
   
-  while( Num != 0)
-  {
-    Dig = Num%10;
-    Num = Num/10;
-    Sum = Sum+Dig;
-  }
-  getch();
-  return Sum;
+      return Sum;
   
 }
