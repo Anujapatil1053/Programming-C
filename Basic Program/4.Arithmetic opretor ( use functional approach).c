e3976b66c89a1a9c198f644aa5b85fab3861c9ba
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-09-02 12:22:39 +0530

    Update 4.Arithmetic opretor ( use functional approach).c

--- Basic Program/4.Arithmetic opretor ( use functional approach).c
+++ Basic Program/4.Arithmetic opretor ( use functional approach).c
@@ -1,34 +1,37 @@
 #include <stdio.h>
 #include<conio.h>
+
 int main()
 {
             int No1, No2, Sum, Mul, Div, Sub, Mod;
 
-            printf("\n\t Enter a 1st number=>");
-            scanf("%d",&No1);
-            printf("\n\t Enter a 2nd number=>");
-            scanf("%d",&No2);
+            printf( " \n \t Enter a 1st number=> " );
+            scanf( " %d ",&No1 );
+            
+            printf( " \n \t Enter a 2nd number=>" );
+            scanf( " %d ",&No2 );
 
-            Sum=No1+No2;
+            Sum = No1  +No2;
 
-            printf("\n Addition of given number%d",Sum);
+            printf( " \n Addition of given number%d",Sum );
 
-            Mul=No1*No2;
+            Mul = No1 * No2;
 
-            printf("\n Multiplication of given number%d",Mul);
+            printf( " \n Multiplication of given number%d",Mul );
 
             Div=No1/No2;
 
-            printf("\n Division of given number%d",Div);
+            printf( " \n Division of given number%d",Div );
 
-            Sub=No1-No2;
+            Sub = No1 - No2;
 
-            printf("\n Subtraction of given number%d",Sub);
+            printf( " \n Subtraction of given number%d",Sub );
 
-            Mod=No1%No2;
+            Mod = No1 % No2;
 
-            printf("\n Mod of given number%d",Mod);
+            printf( " \n Mod of given number%d",Mod );
 
             getch();
+            
             return 0;
 }
