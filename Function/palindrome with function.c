0ffac337ca8cc0f6382fef2aa69b42d8d094a771
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-09-02 12:39:55 +0530

    Update palindrome with function.c

--- Function/palindrome with function.c
+++ Function/palindrome with function.c
@@ -5,36 +5,39 @@ int Palindrome(int);
 
 int main()
 {
-  int No = 0;
-  
-  printf("\n Enter Any Number =>");
-  scanf("%d",&No);
-  
-  Palindrome(No);
-  
-  getch();
+        int No = 0;
+
+        printf("\n Enter Any Number =>");
+        scanf("%d",&No);
+
+        Palindrome(No);
+
+        getch();
 }
 int Palindrome(int Num)
 {
-  int Rev = 0, Dig = 0,No = 0;
+        int Rev = 0, Dig = 0,No = 0;
+
+        No = Num;
   
-  No = Num;
-  while( No>0 )
-  {
-    Dig = No%10;
-    Rev = (Rev*10)+Dig;
-    No = No/10;
-  }
-  if(Num == Rev)
-  {
-    printf("\n Given Number is %d Palindrome",Num);
-  }
-  else
-  {
-    printf("\n Given Number is %d not palindrome",Num);
-  }
-  getch();
-  
-  return;
+        while( No > 0 )
+        {
+              Dig = No % 10;
+          
+              Rev = ( Rev * 10 ) + Dig;
+          
+              No = No / 10;
+        }
+        if( Num == Rev )
+        {
+            printf("\n Given Number is %d Palindrome",Num);
+        }
+        else
+        {
+            printf("\n Given Number is %d not palindrome",Num);
+        }
+        getch();
+
+        return;
   
 }
