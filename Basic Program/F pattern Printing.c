93203b28942d2b36293554a2d06b172dfcb8924b
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-09-02 12:26:15 +0530

    Update F pattern Printing.c

--- Basic Program/F pattern Printing.c
+++ Basic Program/F pattern Printing.c
@@ -2,26 +2,31 @@
 #include<conio.h>
 int main()
 {
-  int r = 0,  c = 0, n = 0;
-  printf(" \n\n Enter No  of Rows & Column Count=" );
-  scanf( "%d",&n );
-  printf(" \n\n\t pattern is =>\n\n" );
-  for ( r = 1; r <= n; r++ )
-  {
-    for( c = 1; c <= n; c++ )
-    {
-      if ( r==1 || c == 1 || c == 1 || r == (n/2+1))
-      {
-        printf(" * ");
-      }
-      else
-      {
-        printf("   ");
-      }
-    }
-    printf("\n");
-  }
-  getch();
-  return 0;
+          int r = 0,  c = 0, n = 0;
+
+          printf(" \n\n Enter No  of Rows & Column Count=" );
+          scanf( "%d",&n );
+
+          printf(" \n\n\t pattern is =>\n\n" );
+
+          for ( r = 1; r <= n; r++ )
+          {
+            for( c = 1; c <= n; c++ )
+            {
+              if ( r==1 || c == 1 || c == 1 || r == (n/2+1))
+              {
+                printf(" * ");
+              }
+              else
+              {
+                printf("   ");
+              }
+            }
+            printf("\n");
+          }
+  
+          getch();
+  
+          return 0;
   
 }
