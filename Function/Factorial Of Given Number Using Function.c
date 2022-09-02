29e41a35ca2d885bfd9aa064b7e9ffc64feeae9b
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-09-02 12:38:24 +0530

    Update Factorial Of Given Number Using Function.c

--- Function/Factorial Of Given Number Using Function.c
+++ Function/Factorial Of Given Number Using Function.c
@@ -6,28 +6,28 @@ long int Factorial ( int );
 
 int main()
 {
-  int No = 0;
-  
-  printf (" \n Enter A Number To Find It's Factorial : ");
-  scanf ( " %d ",&No );
-  
-  printf ( " \n Factorial Of Given Number %d Is = %ld.",No, Factorial ( No ));
-  
-  getch();
-  
-  return 0;
+        int No = 0;
+
+        printf (" \n Enter A Number To Find It's Factorial : ");
+        scanf ( " %d ",&No );
+
+        printf ( " \n Factorial Of Given Number %d Is = %ld.",No, Factorial ( No ));
+
+        getch();
+
+        return 0;
 }
 
  long int Factorial ( int Num )
 {
-  long int Fac = 1;
-  
-  while ( Num > 1 )
-  {
-    Fac = Fac * Num;
-    Num--;
-  }
-  
-  return Fac;
+        long int Fac = 1;
+
+        while ( Num > 1 )
+        {
+              Fac = Fac * Num;
+              Num--;
+        }
+
+        return Fac;
 }
 
