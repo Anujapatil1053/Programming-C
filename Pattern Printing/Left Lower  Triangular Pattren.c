47f6a4c7328df5e3d4f7fb2306d112956a4aa701
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-09-04 21:35:15 +0530

    Add files via upload

---
+++ Pattern Printing/Left Lower  Triangular Pattren.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int i = 0, j = 0, Cnt = 0;
+  
+  printf(" \n Enter No. Of Rows &  Column ");
+  scanf( " %d ",&Cnt );
+  
+  for( i = 1; i <= Cnt; i++ )
+  {
+    for( j = 1; j <= Cnt; j++ )
+    {
+      if(  i >= j)
+      {
+        printf(" * ");
+        
+      }
+      else
+      {
+        printf("   ");
+      }
+    }
+    printf("\n");
+  }
+  getch();
+  return 0;
+  
+}
