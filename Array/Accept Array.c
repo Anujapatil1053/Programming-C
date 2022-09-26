4b4a2bca21cbb016bd0e67699eb967d445ac8d71
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-09-26 18:45:21 +0530

    Add files via upload

---
+++ Array/Accept Array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  int Arr[5] = { 51, 44 ,55 ,77, 87, };
+  
+  printf(" \n Array  Of 0 Element = %d  ",Arr[0] );
+  
+  printf(" \n Array  Of 1 Element = %d  ",Arr[1] );
+  
+  printf(" \n Array  Of 2 Element = %d  ",Arr[2] );
+  
+  printf(" \n Array  Of 3  Element = %d  ",Arr[3] );
+  
+  printf(" \n Array  Of 4  Element = %d  ",Arr[4] );
+  
+  
+  
+  getch();
+  return 0;
+  
+  
+}
