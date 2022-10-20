fc0510573e3225beff270bc1f2769015194f7e39
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-10-20 20:26:16 +0530

    Add files via upload

---
+++ Table Program/02_Program To Print Table In Given Renge.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+    int SP,EP,Cnt,NO;
+   printf("\n\t Enter First Number =>  ");
+   scanf("%d",&SP);
+   
+   printf("\n\t Enter Last Number =>  ");
+   scanf("%d",&EP);
+   
+   for(NO = 1; NO <= 10; NO++)
+   {
+   
+       for(Cnt = SP; Cnt <= EP; Cnt++)
+       {
+           printf(" %-3d",NO * Cnt);
+       }
+       printf("\n");
+       
+    }
+   
+   getch();
+   return 0;
+}
