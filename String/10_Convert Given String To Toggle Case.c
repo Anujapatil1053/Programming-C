56809cc80cbb173acdd37d9a16df1b1226199223
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-10-20 10:11:15 +0530

    Add files via upload

---
+++ String/10_Convert Given String To Toggle Case.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  char cSrc[20] = {'\0'};
+  int i = 0;
+  
+  printf("\n Enter A String =>");
+  gets(cSrc);
+  
+  while(cSrc[i] != '\0')
+  {
+    if(cSrc[i] >= 'A' && cSrc[i] <= 'Z') 
+    {
+      cSrc[i] = cSrc[i] -32;
+    }
+    else if(cSrc[i] >= 'a' && cSrc[i] <= 'z') 
+    {
+      cSrc[i] = cSrc[i] +32;
+    }
+    i++;
+  }
+  printf("\n Given String Is After Converting To Lower = %d",cSrc);
+  
+  getch();
+  return 0;
+}
