f2e64233524b79f5d60808f9df3c72492ef23a40
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-10-21 18:59:08 +0530

    Add files via upload

---
+++ Conditional control statements/17_Even Number Series.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+         int  S_No = 0, E_No = 0;
+
+         printf("\n\t=============*******=============\n");
+
+         printf("\n\t Enter Starting Number = ");
+         scanf("%d",&S_No);
+
+         printf("\n\t Enter Ending Number = ");
+         scanf("%d",&E_No);
+
+         while(S_No <= E_No)
+         {
+                 if(S_No % 2 == 0)
+                 {
+                       printf("\n\t %d",S_No);
+                 }
+                 S_No++;
+         }
+
+         printf("\n\t=============*******=============\n");
+         
+         printf("\n\t\t Thanks !!!\n");
+
+         getch();
+         return 0;
+         
+}
