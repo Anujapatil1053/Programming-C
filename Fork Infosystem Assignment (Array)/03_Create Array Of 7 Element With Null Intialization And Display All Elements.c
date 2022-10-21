60c160f1880e3876937a6c8236ef7916cda04970
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-10-21 17:59:17 +0530

    Add files via upload

---
+++ Fork Infosystem Assignment (Array)/03_Create Array Of 7 Element With Null Intialization And Display All Elements.c
@@ -0,0 +1,21 @@
+// 03.Create Array Of 7 Elements With Null(0) Initialization & Display All Elements.
+
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+        int Arr[7] = {'\0'};
+
+        printf("\n\n The 1st Element = %d",Arr[0]);
+        printf("\n\n The 2nd Element = %d",Arr[1]);
+        printf("\n\n The 3rd Element = %d",Arr[2]);
+        printf("\n\n The 4th Element = %d",Arr[3]);
+        printf("\n\n The 5th Element = %d",Arr[4]);
+        printf("\n\n The 6th Element = %d",Arr[5]);
+        printf("\n\n The 7th Element = %d",Arr[6]);
+
+        getch();
+        return 0;
+}
