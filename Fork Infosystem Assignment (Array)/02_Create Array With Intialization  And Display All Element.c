542295d120a4e0e8e3825bb87ce37840c729e62a
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-10-21 17:55:19 +0530

    Add files via upload

---
+++ Fork Infosystem Assignment (Array)/02_Create Array With Intialization  And Display All Element.c
@@ -0,0 +1,19 @@
+ // 02.Create Array With Initialization & Display All Elements
+
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+        int Arr[5] = {23, 44, 90, 2, 11};
+
+        printf("\n\n The 1st Element = %d",Arr[0]);
+        printf("\n\n The 2nd Element = %d",Arr[1]);
+        printf("\n\n The 3rd Element = %d",Arr[2]);
+        printf("\n\n The 4th Element = %d",Arr[3]);
+        printf("\n\n The 5th Element = %d",Arr[4]);
+
+        getch();
+        return 0;
+}
