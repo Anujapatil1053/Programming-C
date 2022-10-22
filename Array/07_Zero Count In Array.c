0af0db67cd7d8b88cf3e37546dc0708a4db016fb
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-10-22 21:27:48 +0530

    Update 07_Zero Count In Array.c

--- Array/07_Zero Count In Array.c
+++ Array/07_Zero Count In Array.c
@@ -4,25 +4,25 @@
 
 int main()
 {
-        int Arr[7] = {'\0'}, i = 0, Z_Cnt = 0;
+                int Arr[7] = {'\0'}, i = 0, Z_Cnt = 0;
 
-        printf("\n\n Enter Array Elements For Finding Zero Count.");
-        for(i = 0; i < 7; i++)
-        {
-                printf("\n\n Enter %d Element = ",i+1);
-                scanf("%d",&Arr[i]);
-        }
-
-        for(i = 0; i < 7; i++)
-        {
-                if(Arr[i] == 0)
+                printf("\n\n Enter Array Elements For Finding Zero Count.");
+                for(i = 0; i < 7; i++)
                 {
-                    Z_Cnt++;
+                        printf("\n\n Enter %d Element = ",i+1);
+                        scanf("%d",&Arr[i]);
                 }
-        }
-        printf("\n\n The Count Of Zero in Given Array = %d",Z_Cnt);
 
-        getch();
-        return 0;
+                for(i = 0; i < 7; i++)
+                {
+                        if(Arr[i] == 0)
+                        {
+                            Z_Cnt++;
+                        }
+                }
+                printf("\n\n The Count Of Zero in Given Array = %d",Z_Cnt);
+
+                getch();
+                return 0;
 
 }
