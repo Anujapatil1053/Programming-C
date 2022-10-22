7806876487043dea68ffa70148e23942000540f9
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-10-22 21:18:54 +0530

    Update 01_Accept Array Element From User And Then Display All Elements.C

--- Array/01_Accept Array Element From User And Then Display All Elements.C
+++ Array/01_Accept Array Element From User And Then Display All Elements.C
@@ -4,23 +4,23 @@
 
 int main()
 {
-  int i = 0, j = 0 , Value[5] = {};
-  
-  for( i = 0, j = 1 ; i < 5; i++, j++)
-  {
-    printf("\n Enter A Element No %d => ",j);
-    scanf(" %d ",&Value[i]);
-  }
-  
-  getch();
-    
-  printf("\n Element Of Array Are => ");
-  
-  for( i = 0, j = 101; i < 5; i++,j++)
-  {
-    printf("\n Values Of %d Element = %d",j,Value[i]);
-  }
-  
-  getch();
-  return 0;
+      int i = 0, j = 0 , Value[5] = {};
+
+      for( i = 0, j = 1 ; i < 5; i++, j++)
+      {
+        printf("\n Enter A Element No %d => ",j);
+        scanf(" %d ",&Value[i]);
+      }
+
+      getch();
+
+      printf("\n Element Of Array Are => ");
+
+      for( i = 0, j = 101; i < 5; i++,j++)
+      {
+        printf("\n Values Of %d Element = %d",j,Value[i]);
+      }
+
+      getch();
+      return 0;
 }
