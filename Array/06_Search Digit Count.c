13b24a1f6572bc6da513f6ae27a690f59edbe062
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-10-22 21:26:59 +0530

    Update 06_Search Digit Count.c

--- Array/06_Search Digit Count.c
+++ Array/06_Search Digit Count.c
@@ -4,41 +4,37 @@
 
 int main()
 {
-        int r = 0, c = 0,No = 0, D_Cnt = 0,Temp = 0, Search_Dig = 0, Dig = 0;
+                int r = 0, c = 0,No = 0, D_Cnt = 0,Temp = 0, Search_Dig = 0, Dig = 0;
 
-        Up:
+                Up:
 
-        printf("\n Enter Positive Integer Number = ");
-        scanf("%d",&No);
+                printf("\n Enter Positive Integer Number = ");
+                scanf("%d",&No);
 
-        Temp = No;
+                Temp = No;
 
-        printf("\n Enter Number For Counting = ");
-        scanf("%d",&Search_Dig);
+                printf("\n Enter Number For Counting = ");
+                scanf("%d",&Search_Dig);
 
-        if(No <= 0)
-        {
-               printf("\n Invalid Number. Please Enter Positive Integer Number.");
-               getch();
-               system("cls");
-               goto Up;
-        }
-
-
-        while(Temp > 0)
-        {
-                Dig = Temp % 10;
-                if(Dig == Search_Dig)
+                if(No <= 0)
                 {
-                        D_Cnt++;
+                       printf("\n Invalid Number. Please Enter Positive Integer Number.");
+                       getch();
+                       system("cls");
+                       goto Up;
                 }
-                Temp = Temp / 10;
-        }
+                while(Temp > 0)
+                {
+                        Dig = Temp % 10;
+                        if(Dig == Search_Dig)
+                        {
+                                D_Cnt++;
+                        }
+                        Temp = Temp / 10;
+                }
 
-        printf("\n Count Of %d in %d is = %d",Search_Dig,No,D_Cnt);
+                printf("\n Count Of %d in %d is = %d",Search_Dig,No,D_Cnt);
 
-        printf("\n\n Thanks !!!\n");
-
-        getch();
-        return 0;
+                getch();
+                return 0;
 }
