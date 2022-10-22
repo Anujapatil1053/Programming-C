3458c28b975385ed4f88dfb4d9a5ec4a108340bd
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-10-22 21:28:41 +0530

    Update 08_Even Count In Array.c

--- Array/08_Even Count In Array.c
+++ Array/08_Even Count In Array.c
@@ -4,24 +4,24 @@
 
 int main()
 {
-        int Arr[7] = {'\0'}, i = 0, E_Cnt = 0;
+                int Arr[7] = {'\0'}, i = 0, E_Cnt = 0;
 
-        printf("\n\n Enter Array Elements For Finding Even Count.");
-        for(i = 0; i < 7; i++)
-        {
-                printf("\n\n Enter %d Element = ",i+1);
-                scanf("%d",&Arr[i]);
-        }
-
-        for(i = 0; i < 7; i++)
-        {
-                if(Arr[i] % 2 == 0 && Arr[i] > 0)
+                printf("\n\n Enter Array Elements For Finding Even Count.");
+                for(i = 0; i < 7; i++)
                 {
-                    E_Cnt++;
+                        printf("\n\n Enter %d Element = ",i+1);
+                        scanf("%d",&Arr[i]);
                 }
-        }
-        printf("\n\n The Count Of Even Numbers in Given Array = %d",E_Cnt);
 
-        getch();
-        return 0;
+                for(i = 0; i < 7; i++)
+                {
+                        if(Arr[i] % 2 == 0 && Arr[i] > 0)
+                        {
+                            E_Cnt++;
+                        }
+                }
+                printf("\n\n The Count Of Even Numbers in Given Array = %d",E_Cnt);
+
+                getch();
+                return 0;
 }
