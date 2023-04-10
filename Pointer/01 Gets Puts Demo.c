ec2dc922c2ae93eacb7e807d6127afe3e8c105c9
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2023-04-10 20:11:58 +0530

    Add files via upload

---
+++ Pointer/01 Gets Puts Demo.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char cArr[50] = {'\0'};
+
+    puts("Enter A String");
+    gets(cArr);
+
+    puts("Given A String");
+    puts(cArr);
+
+    getch();
+    return 0;
+
+
+}
